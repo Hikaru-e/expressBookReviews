e0ae51ea3cd7f82dd1af8f75b0e446428d4a157e
Oussama El Akhiri <oussama.elakhiri@usms.ac.ma>
2024-06-04 20:09:21 +0100

    listing books with async & searching books by ISBN w/ Promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,25 +23,29 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
+public_users.get('/', async (req, res) => {
   return res.status(200).json(books);
-  // return res.status(200).json(books);
-  // return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
+// Get book details based on ISBN using Promises
+public_users.get('/isbn/:isbn', (req, res) => {
   const isbn = req.params.isbn;
 
-  if (!books[isbn]) {
-      return res.status(404).json({ message: "Book not found" });
-  }
-
-  return res.status(200).json(books[isbn]);
-  // return res.status(300).json({message: "Yet to be implemented"});
- });
+  new Promise((resolve, reject) => {
+    const book = books[isbn];
+    if (book) {
+      resolve(book);
+    } else {
+      reject("Book not found");
+    }
+  })
+  .then(book => {
+    res.status(200).json(book);
+  })
+  .catch(error => {
+    res.status(404).json({ message: error });
+  });
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
